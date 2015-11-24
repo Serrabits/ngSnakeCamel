ff706cab32c5e1033c3c6128ba4d2ee76a81d79f
Junior Conte <juniorcnt@gmail.com>
2015-11-24 02:10:10 -0200

    Configure TravisCI

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,14 +2,20 @@
 // Generated on Tue Nov 24 2015 00:13:56 GMT-0200 (BRST)
 
 module.exports = function(config) {
+  'use strict';
+
   config.set({
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '..',
+    basePath: '../',
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: [
+      'jasmine'
+    ],
 
     // list of files / patterns to load in the browser
     files: [
@@ -22,40 +28,33 @@ module.exports = function(config) {
     // list of files to exclude
     exclude: [
     ],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+  
     // web server port
-    port: 9876,
+    port: 8080,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: [
+      'Chrome'
+    ],
+
+    // Which plugins to enable
+    plugins: [
+      "karma-chrome-launcher",
+      "karma-phantomjs-launcher",
+      "karma-jasmine"
+    ],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
 
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_INFO
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultanous
-    concurrency: Infinity
   });
 };
